7ad30c45440a1f9b5b7c7ac1484bceca1ade05e1
Cojim <christopherorjiministries@gmail.com>
2023-02-21 07:49:09 -0800

    Updated the payment page

--- src/navPages/give/International.js
+++ src/navPages/give/International.js
@@ -73,7 +73,7 @@ const International = () => {
     return (
         <>
             <Navbar />
-            <section className="md:flex md:flex-row mx-2 flex-col justify-center">
+            <section className="md:flex md:flex-row mx-2 flex-col justify-center mt-2">
                 <button onClick={() => setfilter('GLOBALPAY')} type="button" className="text-primary bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200 mr-2 mb-2">
                     <img alt="Globay Pay" src="https://demo.globalpay.com.ng/GlobalPayAPI/img/globalpay.jpg" className="w-20 h-4" />
                 </button>
@@ -111,7 +111,7 @@ const International = () => {
                                 <section className="bg-[#F3F4F6] pt-4 pb-10 px-4">
                                     <div className="container mx-auto">
                                         <div className="-mx-4 flex flex-wrap">
-                                            <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+                                            <div className="w-full px-4 md:w-1/2 xl:w-1/4">
                                                 <div className="mb-10 overflow-hidden rounded-lg bg-white">
                                                     <img
                                                         src="https://upload.wikimedia.org/wikipedia/commons/3/3e/Ecobank_Logo_EN.png"
@@ -142,7 +142,7 @@ const International = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+                                            <div className="w-full px-4 md:w-1/2 xl:w-1/4">
                                                 <div className="mb-10 overflow-hidden rounded-lg bg-white">
                                                     <img
                                                         src="https://businessday.ng/wp-content/uploads/2022/02/UBA-8-1.png"
@@ -173,7 +173,7 @@ const International = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+                                            <div className="w-full px-4 md:w-1/2 xl:w-1/4">
                                                 <div className="mb-10 overflow-hidden rounded-lg bg-white">
                                                     <img
                                                         src="https://i2.wp.com/kleenriteltd.com/wp-content/uploads/2014/11/zenith-bank.jpg?fit=750%2C350&ssl=1"
@@ -204,7 +204,7 @@ const International = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+                                            <div className="w-full px-4 md:w-1/2 xl:w-1/4">
                                                 <div className="mb-10 overflow-hidden rounded-lg bg-white">
                                                     <img
                                                         src="https://cdn.punchng.com/wp-content/uploads/2022/06/09021310/First-Bank.png"
@@ -380,13 +380,12 @@ const International = () => {
                                                     </g>
                                                 </svg>
                                                 <select name="currency" className="text-2xl w-64 font-bold rounded border-2 border-pribg-primary text-gray-600 h-14 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
-                                                    <option>NGN</option>
                                                     <option>USD</option>
                                                 </select>
                                             </div>
 
                                             <input type="hidden" id="merch_txnref" name="merch_txnref" value={makeid(50)}></input>
-                                            <input type="hidden" id="merchantid" name="merchantid" value="22979" />
+                                            <input type="hidden" id="merchantid" name="merchantid" value="22980" />
                                         </div>
 
 

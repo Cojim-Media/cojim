18a0861abf3d87ca3951951987adf324511978d1
Cojim <christopherorjiministries@gmail.com>
2023-03-04 09:07:45 -0800

    Completed media link upload

--- src/navPages/about us/About.js
+++ src/navPages/about us/About.js
@@ -8,17 +8,17 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <section class="pt-16 bg-blueGray-50">
-        <div class="mx-auto">
-          <div class="flex flex-wrap items-center">
-            <div class="md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
-              <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-500">
-                <img alt="..." src={logo} class="w-full align-middle rounded-t-lg" />
-                <blockquote class="relative p-6 mb-4">
-                  {/* <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block h-95-px -top-94-px">
-                      <polygon points="-30,95 583,95 583,65" class="text-blue-500 fill-current"></polygon>
+      <section className="pt-16 bg-blueGray-50">
+        <div className="mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-500">
+                <img alt="..." src={logo} className="w-full align-middle rounded-t-lg" />
+                <blockquote className="relative p-6 mb-4">
+                  {/* <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute left-0 w-full block h-95-px -top-94-px">
+                      <polygon points="-30,95 583,95 583,65" className="text-blue-500 fill-current"></polygon>
                     </svg> */}
-                  <h4 class="text-xl font-bold text-white">
+                  <h4 className="text-xl font-bold text-white">
                     Our Mission Statement.
                   </h4>
                   <p className="text-md font-light mt-2 text-white">

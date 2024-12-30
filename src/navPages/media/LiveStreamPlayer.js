8a9ec10a8e5cbc8ccd2e81a3892da5abddcb672d
Petec0x0 <udehonyedikachi01@gmail.com>
2024-12-30 16:51:04 +0000

    Added Websocket to the live chat functionality

--- src/navPages/media/LiveStreamPlayer.js
+++ src/navPages/media/LiveStreamPlayer.js
@@ -7,88 +7,9 @@ import Navbar from '../../components/navbar/Navbar';
 import { useParams } from 'react-router-dom';
 import MediaList from './MediaList';
 import './media.css'
-import { FaRegPaperPlane } from "react-icons/fa6";
-
-const LiveChat = ({ roomId }) => {
-    const [messages, setMessages] = useState([]); // Store chat messages
-    const [inputMessage, setInputMessage] = useState(''); // Store user input
-
-    // Fetch dummy data including photos when the component loads
-    useEffect(() => {
-        axios.get(`/api/live-chat/history/${roomId}`)
-            .then(response => {
-                const chats = response.data; // Get only a few dummy messages with photos
-                console.log(chats)
-                setMessages(chats);
-            })
-            .catch(error => console.error('Error fetching messages:', error));
-    }, [roomId]);
-
-    // Function to handle sending a new message
-    const sendMessage = async () => {
-        if (inputMessage.trim() !== '') {
-            const user = localStorage.getItem('user');
-            if (user !== null) {
-                setMessages([...messages, { message: inputMessage, userId: JSON.parse(user) }]); // Add new message to state
-                setInputMessage(''); // Clear input field
-            } else {
-                alert("Signin to Chat");
-            }
-
-            await fetch('/api/live-chat/send', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    roomId,
-                    message: inputMessage,
-                })
-            });
-        }
-    };
-
-    return (
-        <div className="bg-slate-200 p-5 mb-2  rounded pr-4 w-[97%]   h-[550px] ">
-            <h3 className="font-bold text2lx">Chat</h3>
-            <div className="rounded mt-2 bg-slate-100 h-96  pr-4 overflow-y-scroll flex flex-col-reverse mb-2">
-                <div className="chat-Container">
-                    {messages.map((message, index) => (
-                        <div key={index} className="chat-message">
+import LiveChat from './LiveChat';
 
 
-                            <div style={{
-                                backgroundImage: `url(${message.userId.passportPhoto.url})`,
-                                backgroundSize: 'cover', // Ensures the image covers the entire div
-                                backgroundPosition: 'center', // Centers the image within the div  
-                                color: 'white', // Ensure text color contrasts with the background
-                                borderRadius: '10px', // Optional: Rounded corners for the message
-                                marginBottom: '10px', // Spacing between messages
-                                width: '20px',
-                                height: '20px'
-                            }} src={message.userId.passportPhoto.url}   >
-
-                            </div>
-                            <div className='message_Title'>
-                                <b>{message.userId.firstname + " " + message.userId.lastname}</b> {message.message}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className='inputchatT'>
-                <input
-                    type="text"
-                    value={inputMessage}
-                    onChange={(e) => setInputMessage(e.target.value)}
-                    placeholder="Type a message..."
-                />
-                <button onClick={sendMessage}><FaRegPaperPlane /></button>
-            </div>
-        </div>
-    );
-};
 
 
 const LiveStreamPlayer = () => {
